ba09f05b20562d7c25c7854a44aea46ff3bb0066
Duy Nguyn <huynhduytam2210@gmail.com>
2021-09-04 16:46:17 +0700

    deadlines

--- client/client.js
+++ client/client.js
@@ -2,17 +2,34 @@ const calculators = require('../server/protos/calculator_pb');
 const service = require('../server/protos/calculator_grpc_pb');
 const grpc = require('@grpc/grpc-js');
 
+function getRPCDeadline(rpcType) {
+    let timeAllowed = 5000;
+    switch (rpcType) {
+        case 1:
+            timeAllowed = 1000;
+            break;
+        case 2:
+            requestAnimationFrame = 7000
+        default:
+            console.log(`Invalid  RPC Type : Using default timeout!`);
+    }
+
+    return new Date(Date.now() + timeAllowed);
+}
+
 async function errorCall() {
+    const deadline = getRPCDeadline(1);
+
     console.log(`gRPC client`);
     const client = new service.CalculatorServiceClient(
         'localhost:50051',
         grpc.credentials.createInsecure()
     );
 
-    const number = -1;
+    const number = 1;
     const squareRootRequest = new calculators.SquareRootRequest();
     squareRootRequest.setNumber(number);
-    client.squareRoot(squareRootRequest, (error, response) => {
+    client.squareRoot(squareRootRequest, { deadline }, (error, response) => {
         if(!error) {
             console.log(response.getNumberRoot());
         } else {
